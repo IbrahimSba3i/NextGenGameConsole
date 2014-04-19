e21ab1db2ed59c3949bcb6eb479a1688d8640d24
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2014-04-19 19:50:17 +0200

    apu

---
+++ apu.v
@@ -0,0 +1,29 @@
+/*	 ___________________________________________
+	|											|
+	|			   _    ____  _   _ 			|
+	|			  / \  |  _ \| | | |			|
+	|			 / _ \ | |_) | | | |			|
+	|			/ ___ \|  __/| |_| |			|
+	|		   /_/   \_|_|    \___/ 			|
+	|		The Audio Processing Unit			|
+	|											|
+	|___________________________________________|
+	
+	
+	The NES APU has 5 programmable sound generators. The base frequency used to generate all sounds is the CPU frequency
+
+
+
+
+
+
+
+*/
+`define BYTE reg[7:0]
+
+module apu();
+	BYTE cRegs[0:21];		/*
+								
+							*/
+	
+endmodule
